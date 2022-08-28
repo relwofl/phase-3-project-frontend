49e12b529e9231391b353ecf5811ce1ebafe9ab3
Luke <luciusfow@gmail.com>
2022-08-28 18:10:37 -0400

    cows get request

--- src/components/Cows.js
+++ src/components/Cows.js
@@ -1,7 +1,19 @@
+
+import { useEffect, useState } from 'react'
 function Cows(){
+    const [cows, setCows ] = useState([])
+
+    useEffect(() => {
+        fetch ('http://localhost:9292/cows')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+    }, [])
+
     return (
         <div>
-            
+            <h1>ANy Cows here?</h1>
         </div>
     )
 }
