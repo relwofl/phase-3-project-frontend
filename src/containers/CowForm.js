359480110e5f175fcc9b9b4f3f0b883390e72c6c
Luke <luciusfow@gmail.com>
2022-09-13 22:57:36 -0400

    updateForm 2

--- src/containers/CowForm.js
+++ src/containers/CowForm.js
@@ -1,33 +1,60 @@
 import React, {useState} from 'react'
+import { useHistory } from 'react-router-dom'
+
 
 function CowForm(){
-    const [ name, setName ] = useState("")
-    const [ mother, setMother ] = useState("")
+    const [ cow, setCow ] = useState({
+        name: "",
+        motherId: "",
+        farmFieldId: ""
+    })
+        const history = useHistory()
 
+    // const [ mother, setMother ] = useState("")
+
+    const handleChange = (e) => {
+        setCow({
+            ...cow,
+            [e.target.name]: e.target.value
+        })
+    }
     function handleSubmit (e){
         e.preventDefualt()
-        const newCow = {name, mother}
-        fetch('http://localhost:9292/cows/', {
+        const newCow = {
+            name: cow.name, 
+            motherId: cow.motherId,
+            farmFieldId: cow.farmFieldId
+        
+        }
+        fetch("http://localhost:9292/cows/", {
             method: "POST",
-            body: JSON.stringify({
-                name: name,
-                mother: mother
-            }),
-        })
-        .then((r) => r.json())
-        .then((newCow) => {
-        console.log(newCow)
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newCow)
+        //         name: name,
+        //         mother: mother
+        //     }),
+        // })
+        // .then((r) => r.json())
+        // .then((newCow) => {
+        // console.log(newCow)
          
         });
+        history.push("/http://localhost:3000/cows")
     }
     return(
-        <div>
-           <h2>Create a cow </h2>
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <>
+        <div>Create a new Cow</div>
+          <form onSubmit={handleSubmit}>
             <label>Cow</label>
-            <input type="text" value={mother} onChange={(e) => setMother(e.target.value)} />
+            <input type="text" name="name" onChange={handleChange} value={cow.name} />
             <label>Mother</label>
-        </div>
+            <input type="integer" name="motherId" onChange={handleChange} value={cow.motherId} />
+            <label>Farm Field</label>
+            <input type="integer" name="farmFieldId" onChange={handleChange} value={cow.farmFieldId} />
+            
+            <input type="submit" value="Add Cow" />
+            </form>
+        </>
     )
 }
 
